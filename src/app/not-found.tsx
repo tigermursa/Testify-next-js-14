57816d9897a9519fe7cbbadb7a04d3dc750d2889
Mursalin Hossain <mursalin2311@gmail.com>
2024-03-30 05:07:44 +0600

    "created next js not found page"

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+const NotFound = () => {
+  return (
+    <div className="flex h-screen justify-center items-center text-2xl font-semibold">
+      <h1>404! not found this page ! maybe developers working on it</h1>
+    </div>
+  );
+};
+
+export default NotFound;
