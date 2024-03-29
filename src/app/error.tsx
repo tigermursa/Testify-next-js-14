d5d19ad2b55204d04b91e9c9f8b98352bcea3c65
Mursalin Hossain <mursalin2311@gmail.com>
2024-03-30 05:05:57 +0600

    "error page created"

---
+++ src/app/error.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const ErrorPage = () => {
+  return (
+    <div className="flex h-screen justify-center items-center text-2xl font-semibold">
+      <h1>Something went wrong!</h1>
+    </div>
+  );
+};
+
+export default ErrorPage;
